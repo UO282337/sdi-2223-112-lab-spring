d5c7f1559aab8c2927e34b06d0a81c90d5bfdf7c
UO282337 <UO282337@uniovi.es>
2023-02-15 20:21:24 +0100

    SDI-2223-112-3.3-Acceso a datos con entidades relacionadas.

--- src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
@@ -2,6 +2,7 @@ package com.uniovi.notaneitor.controllers;
 
 import com.uniovi.notaneitor.entities.Mark;
 import com.uniovi.notaneitor.services.MarksService;
+import com.uniovi.notaneitor.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,11 @@ import org.springframework.web.bind.annotation.*;
 public class MarksController {
     @Autowired //Inyectar el servicio
     private MarksService marksService;
+
+    // Inyectamos el servicio
+    @Autowired
+    private UsersService usersService;
+
 
     @RequestMapping("/mark/list")
     public String getList(Model model) {
@@ -46,22 +52,26 @@ public class MarksController {
         return "redirect:/mark/list";
     }
 
-    @RequestMapping(value = "/mark/add")
-    public String getMark() {
+    // Modificamos los siguientes metodos
+    @RequestMapping(value="/mark/add")
+    public String getMark(Model model){
+        model.addAttribute("usersList", usersService.getUsers());
         return "mark/add";
     }
-
     @RequestMapping(value = "/mark/edit/{id}")
     public String getEdit(Model model, @PathVariable Long id) {
         model.addAttribute("mark", marksService.getMark(id));
+        model.addAttribute("usersList", usersService.getUsers());
         return "mark/edit";
     }
-
-    @RequestMapping(value="/mark/edit/{id}", method=RequestMethod.POST)
-    public String setEdit(@ModelAttribute Mark mark, @PathVariable Long id){
-        mark.setId(id);
-        marksService.addMark(mark);
-        return "redirect:/mark/details/"+id;
+    @RequestMapping(value = "/mark/edit/{id}", method = RequestMethod.POST)
+    public String setEdit(@ModelAttribute Mark mark, @PathVariable Long id) {
+        Mark originalMark = marksService.getMark(id);
+        // modificar solo score y description
+        originalMark.setScore(mark.getScore());
+        originalMark.setDescription(mark.getDescription());
+        marksService.addMark(originalMark);
+        return "redirect:/mark/details/" + id;
     }
 
     @RequestMapping("/mark/list/update")
